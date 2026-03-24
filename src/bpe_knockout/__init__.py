d778860affb403f8d2738d530b17aff2648b1aac
Thomas Bauwens <thomas.bauwens@kuleuven.be>
2026-03-24 12:15:08 +0100

    🔖 Release v2026.03.24
    - pip suffix [github] is now [full].
    - ReleaseMe: Created GitHub Actions workflow for PyPI publishing.
    - ReleaseMe: Updated GitHub Actions workflow for PyPI publishing.
    - ReleaseMe: Updated GitHub Actions workflow for PyPI publishing.
    - Moved TkTkT dependency to PyPI.
    - 📣 ReleaseMe: Updated GitHub Actions workflow for PyPI publishing.

--- src/bpe_knockout/__init__.py
+++ src/bpe_knockout/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2026.03.13"
+__version__ = "v2026.03.24"
 
 from .model.config import FullBPEKnockoutConfig, KnockoutConfig, AnnealingConfig, ReifyConfig, ReferenceMode, ReifyMode, AnnealingTime
 from .model.tokeniser import BTE
